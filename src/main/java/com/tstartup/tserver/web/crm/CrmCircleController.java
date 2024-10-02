683782394711bbd002f3f7bdbfed3b2dcff73739
JAY199\zhang <zhangjie7577@163.com>
2024-10-02 17:13:43 +0800

    feat-init

---
+++ src/main/java/com/tstartup/tserver/web/crm/CrmCircleController.java
@@ -0,0 +1,11 @@
+package com.tstartup.tserver.web.crm;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "后台-Circle")
+@RestController
+@RequestMapping("/crm/article")
+public class CrmCircleController {
+}
